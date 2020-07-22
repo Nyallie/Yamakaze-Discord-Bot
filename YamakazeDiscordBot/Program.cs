591a822f8315f915490d060fbac25e6541ff6a52
Théo LEBAILLIF <theolebaillif24@gmail.com>
2020-07-22 02:42:46 +0200

    Mise en place de protection. Mise en place de if pour éviter un crash de l'application si le fichier config.json n'existe pas

--- YamakazeDiscordBot/Program.cs
+++ YamakazeDiscordBot/Program.cs
@@ -24,25 +24,40 @@ namespace YamakazeDiscordBot
         
         public async Task MainAsync()
         {
-            _json = JObject.Parse(File.ReadAllText("Properties/config.json"));
-            _client = new DiscordSocketClient();
-            _commands = new CommandService();
-            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
-            _commandhandler = new CommandHandler(_client,_commands,_json);
-            _client.Log += Log;
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            string token = _json.GetValue("token").ToString();
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-            await _commandhandler.InstallCommandsAsync();
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
+            if (!File.Exists("Properties/config.json"))
+            {
+                Console.WriteLine("Le fichier config.json est inexistant. Le bot ne peux pas fonctionner sans se fichier.");
+            }
+            else
+            {
+                _json = JObject.Parse(File.ReadAllText("Properties/config.json"));
+                _client = new DiscordSocketClient();
+                _commands = new CommandService();
+                _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+                _commandhandler = new CommandHandler(_client, _commands, _json);
+                _client.Log += Log;
+                //  You can assign your bot token to a string, and pass that in to connect.
+                //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+                string token = _json.GetValue("token").ToString();
+                if (token.Equals(""))
+                {
+                    Console.WriteLine("Token non renseigner dans le fichier Properties/config.json. \n" +
+                        "Veuillez renseignez votre token et relancer le bot");
+                }
+                else
+                {
+                    // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+                    // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+                    // var token = File.ReadAllText("token.txt");
+                    // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+                    await _commandhandler.InstallCommandsAsync();
+                    await _client.LoginAsync(TokenType.Bot, token);
+                    await _client.StartAsync();
 
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
+                    // Block this task until the program is closed.
+                    await Task.Delay(-1);
+                }
+            }
         }
 
         private Task Log(LogMessage msg)
