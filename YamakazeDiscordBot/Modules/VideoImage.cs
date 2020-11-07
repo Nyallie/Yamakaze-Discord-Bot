c1776e95a843721dec5650de79fac90378ae60b8
Heliodore24 <53257868+Heliodore24@users.noreply.github.com>
2020-11-07 12:59:50 +0100

    Mise a jour de la commande hololive. - Ajout d'un Embed de transition le temps que le json sois traiter. - une fois que le json est traité, modification du premier Embed par un nouveau avec toutes les information de la vidéo.

--- YamakazeDiscordBot/Modules/VideoImage.cs
+++ YamakazeDiscordBot/Modules/VideoImage.cs
@@ -15,38 +15,49 @@ namespace YamakazeDiscordBot.Modules
 {
     public class VideoImage : ModuleBase<SocketCommandContext>
     {
-        private string _url = "https://homoliver.live/api/v1/allvideos?limit=1&offset=";
+        private string _urlhololive = "https://api.homoliver.live/api/hololive/v1/allvideos?limit=1&offset=";
         private Color _color = new Color(39, 198, 220);
 
+        //Ne fonctionne plus car l'api utiliser n'existe plus
         [Command("hololive")]
         [Summary("Get a random video from hololive")]
         public async Task Hololive()
         {
+            EmbedBuilder embedBuilderLoading = new EmbedBuilder()
+                .WithTitle("Serching a random video...")
+                .WithImageUrl("https://thumbs.gfycat.com/LittleBestAmoeba-max-14mb.gif")
+                .WithColor(_color);
+            Embed embedloading = embedBuilderLoading.Build();
+            var message = await ReplyAsync(embed: embedloading);
+            //Recherche de la vidéo
             bool sucess = true;
             Random rnd = new Random();
             Requethttp req = new Requethttp();
-            DataJsonHololive hololive = await req.GetDataJsonHololive(_url + rnd.Next(1,4968));
-            EmbedBuilder embedBuilder = new EmbedBuilder();
+            DataJsonHololive hololive = await req.GetDataJsonHololive(_urlhololive + rnd.Next(1,4968));
+            EmbedBuilder embedBuilderfinal = new EmbedBuilder();
             if (hololive == null)
             {
-                embedBuilder.WithTitle("Command error")
+                embedBuilderfinal.WithTitle("Command error")
                     .WithDescription("The API doesn't response succesfuly.");
                 sucess = false;
             }
             else
             {
                 VideoHololive vid = hololive.Result[0];
-                string url = vid.VideoId;
-                string vidid = vid.VideoId.Remove(0, 32);
-                embedBuilder.WithTitle("Random Hololive video")
+                string url = "https://www.youtube.com/watch?" + vid.VideoId;
+                string vidid = vid.VideoId;
+                embedBuilderfinal.WithTitle("Random Hololive video")
                     .AddField(vid.VideoName, vid.Vtuber + "\n" + url)
                     .WithUrl(url)
                     .WithColor(_color)
                     .WithImageUrl($"https://img.youtube.com/vi/{vidid}/hqdefault.jpg");
             }
-            Embed embed = embedBuilder.Build();
-            await ReplyAsync(embed: embed);
+            Embed embed = embedBuilderfinal.Build();
+            await message.ModifyAsync(msg => msg.Embed = embed);
+            //await ReplyAsync(embed: embed);
             Program.log.WriteCommandToConsole(Context.User.ToString(), "hololive"+" Réussi : "+sucess);
         }
+
+
     }
 }
